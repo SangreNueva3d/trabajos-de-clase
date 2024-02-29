e11dfe596bd410376533ae3b201fe8623c00f660
SangreNueva3d <angel.yepes2@udea.edu.co>
2024-02-28 21:52:02 -0500

    inicializando proyecto

---
+++ build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    id("java")
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+// https://mvnrepository.com/artifact/org.apache.logging.log4j/log4j-core
+    implementation("org.apache.logging.log4j:log4j-core:3.0.0-beta2")
+// https://mvnrepository.com/artifact/org.apache.logging.log4j/log4j-api
+    implementation("org.apache.logging.log4j:log4j-api:3.0.0-beta2")
+
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
