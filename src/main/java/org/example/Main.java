0126c09bdcf14e5c1ed23b541af3648bee7d8326
SangreNueva3d <angel.yepes2@udea.edu.co>
2024-02-29 21:38:34 -0500

    inicializando proyecto

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,14 +5,13 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.printf("Hello and welcome!");
-        System.out.printf("Holaa!");
+        System.out.println("Hello and welcome!");
+        System.out.println("Holaa!");
         int a = 3;
         int b = 55;
 
         int s = a + b;
-        System.out.printf(s);
+        System.out.println(s);
 
         }
     }
-}
